12a97474e44cd931bb94550831691f9ce93e2faa
trananhquan2002 <trananhquan23702@gmail.com>
2022-03-25 09:19:00 +0700

    quanlybanhang

---
+++ trangchu.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Trang chủ</title>
+</head>
+<body>
+	<h1>Chào mừng bạn đến với singapore Thành phố đáng sống</h1>
+	<img src="img/singapore.jpg" alt="Singapore" width="800px">
+	<h2><span style="font-weight: 400;">1, Giới thiệu tổng quan về đất nước Singapore</span></h2>
+	<p style="text-align: justify;"><span style="font-weight: 400;">Singapore còn có tên gọi khác là Quốc đảo Sư Tử. Sở dĩ có cái tên này chính là do chữ Singapore xuất phát từ chữ Singapura trong tiếng Malaysia.</span></p>
+	<p style="text-align: justify;"><span style="font-weight: 400;">Xét theo nguồn gốc chữ Phạn, Singapura được ghép bởi 2 từ là Siga (Sư tử) và Pura ( Thành phố). Do đó mà đất nước này có tên gọi khác là Quốc đảo Sư Tử.</span></p>
+	<h3 style="text-align: justify;"><span style="font-weight: 400;">Vị trí địa lý</span></h3>
+	<p style="text-align: justify;"><span style="font-weight: 400;">Singapore là một hòn đảo có hình dáng của một viên kim cương. Nằm ngoài khơi mũi phía nam của bán đảo Mã lai và cách xích đạo 137km về phía Bắc.</span></p>
+	<p style="text-align: justify;"><span style="font-weight: 400;">Singapore gồm 1 đảo chính và 63 đảo nhỏ xung quanh. Phía Bắc giáp với eo biển Johor (tách biệt với Malaysia), phía Nam giáp với quần đảo Riau của Indonesia qua eo biển Singapore. Thủ đô của đất nước này chính là thành phố Singapore.</span></p>
+	<h3 style="text-align: justify;"><span style="font-weight: 400;">Diện tích và dân số đất nước Singapore</span></h3>
+	<p style="text-align: justify;"><span style="font-weight: 400;">Singapore chỉ là một quốc đảo nhỏ nên diện tích toàn đất nước chỉ có 697km2 bao gồm đảo chính 585,4km2. Còn diện tích còn lại là của 63 đảo nhỏ nằm rải rác xung quanh.</span></p>
+	<p style="text-align: justify;"><span style="font-weight: 400;">Chính vì diện tích nhỏ nên dân số của Singapore cũng chỉ khoảng hơn 6.000.000 người ( 2020)</span></p>
+	<h3 style="text-align: justify;"><span style="font-weight: 400;">Ngôn ngữ sử dụng ở đất nước singapore</span></h3>
+	<p style="text-align: justify;"><span style="font-weight: 400;">Ở Singapore đang có 4 ngôn ngữ đang được sử dụng đó chính là tiếng Anh, tiếng Mã Lai, tiếng Hoa và tiếng Tamil. Có một điều đặc biệt là người dân ở đây có thể nói được từ 2 thứ tiếng trở lên, nhiều nhất là tiếng Anh và tiếng Hoa.</span></p>
+	<p style="text-align: justify;"><span style="font-weight: 400;">Trong đó, tiếng Anh là ngôn ngữ được chú trọng nhất, gần như là chính thức của đất nước này. Tiếng Anh được sử dụng trong giáo dục và giao thương với các nước khác.</span></p>
+	<p style="text-align: justify;"><span style="font-weight: 400;">Đây cũng chính là lý do vì sao việc sinh sống, học tập và làm việc của người ngoại quốc tại đất nước này khá dễ dàng. Điều này tạo nên sự thu hút các du học sinh từ nhiều nước trên thế giới.</span></p>
+	<p style="text-align: justify;"><span style="font-weight: 400;">Đơn vị tiền tệ của Singapore là Đô la Singapore viết tắt là SGD.</span></p>
+	<h3 style="text-align: justify;"><span style="font-weight: 400;">Khí hậu ở Singapore</span></h3>
+	<p style="text-align: justify;"><span>Về phần khí hậu thì ở Singapore chủ yếu là khí hậu xích đạo. Do chỉ nằm cách đường xích đạo 137km về phía Bắc. Do đó mà khí hậu của đất nước này tương đối ổn định. Không phân chia thành 4 mùa mà chỉ phân ra thành 2 mùa.</span></p>
+	<p style="text-align: justify;"><span>Đó là mùa nóng ( từ tháng 4 – 10, tháng 5, 6 là tháng nóng nhất) và mùa lạnh ( từ tháng 11 → tháng 3 năm sau). Trong đó tháng 12, và tháng 1 là lạnh nhất.</span></p>
+</body>
+</html>
